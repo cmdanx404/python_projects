3145cd9c69848362d1ac9448e866fa63992c6c45
danielmadjus <danielmadjus@gmail.com>
2025-08-10 23:17:28 +0800

    Add files via upload

---
+++ project21/README.md
@@ -0,0 +1,8 @@
+Instruction:
+Create a program which asks for names and select random name from the list of names. The person selected willh have to pay for everybody's food bill.
+
+Example Input:
+Matthew, Mark, Luke, John
+
+Example Output:
+Mark is going to pay for the meal today
---
+++ project21/bill_roullete.py
@@ -0,0 +1,10 @@
+import random
+
+names_string = input("Input everyone's name, seperated by comma. ")
+names = names_string.split(", ")
+
+num_items = len(names)
+random_int = random.randint(0, num_items - 1)
+person_name = names[random_int]
+
+print(f"{person_name} is going to buy the meal today!")
